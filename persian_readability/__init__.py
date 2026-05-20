c48e0b20f7ab71733db3d43bbba637f4c7fe9067
Mohammad Pirouzan <mohammadpirouzan@gmail.com>
2026-05-20 18:19:14 +0000

    Bump version to 0.1.2

--- persian_readability/__init__.py
+++ persian_readability/__init__.py
@@ -10,7 +10,7 @@ for _name in dir(_core):
     if not _name.startswith("__"):
         globals()[_name] = getattr(_core, _name)
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 class PersianReadability:
